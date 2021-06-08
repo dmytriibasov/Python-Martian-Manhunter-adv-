a1f9aaeeda36ebb67da7868d26e1f715dfcff8f0
Dmytrii Basov <dimon4a2009@gmail.com>
2021-06-08 15:03:39 +0300

    Homework5 modified

--- my_homeworks/Homework5/test_game_pytest.py
+++ my_homeworks/Homework5/test_game_pytest.py
@@ -1,22 +1,27 @@
 import pytest
 from game import TicTacToe
 
-game = TicTacToe()
+game = None
 
-game.board[3] = 'X'
-game.board[7] = 'O'
+
+def setup_module(module):
+    global game
+    game = TicTacToe()
+    game.board[3] = 'X'
+    game.board[7] = 'O'
 
 
 def test_move_available():
     assert 3 not in game.available_moves()
     assert 7 not in game.available_moves()
-    assert 4 in game.available_moves()
+    assert 4 in gamHome.available_moves()
 
 
 def test_make_move():
     assert game.make_move(square=6, letter='X') is True
     assert game.make_move(square=7, letter='X') is False
     assert game.make_move(square=3, letter='O') is False
+    assert game.make_move(square=8, letter='O') is True
 
     with pytest.raises(IndexError):
         assert game.make_move(9, 'X')
@@ -24,5 +29,3 @@ def test_make_move():
     with pytest.raises(TypeError):
         assert game.make_move(square='X', letter=2)
         assert game.make_move(square=3, letter=2)
-
-
