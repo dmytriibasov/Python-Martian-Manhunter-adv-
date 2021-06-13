19f700dbc54a8d68d54a984fd2831cf0baeefa33
Dmytrii Basov <dimon4a2009@gmail.com>
2021-06-13 17:11:19 +0300

    Homework13, Task4 - done.

--- my_homeworks/Homework13/flask/app.py
+++ my_homeworks/Homework13/flask/app.py
@@ -13,22 +13,40 @@ def homepage():
 
 @app.route('/search', methods=['POST'])
 def search_weather():
-    city = request.form.get("city")
-    querystring = {"q": city, "cnt": "1", "mode": "null", "lon": "0", "type": "link, accurate", "lat": "0",
-                   "units": "metric"}
 
-    headers = {
-        'x-rapidapi-key': Config.WEATHER_API_KEY,
-        'x-rapidapi-host': Config.WEATHER_API_HOST
-    }
+    cities = request.form.getlist("cities")
+    weather_data = []
+    for index, city in enumerate(cities):
+        print(f'\n{index}: {city}\n')
+        querystring = {"q": city, "cnt": "1", "mode": "null", "lon": "0", "type": "link, accurate", "lat": "0",
+                       "units": "metric"}
 
-    response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
-    if response.status_code == 200:
-        data = response.json()
-        weather = data['list'][0]
-        return render_template("weather.html", weather=weather)
+        headers = {
+            'x-rapidapi-key': Config.WEATHER_API_KEY,
+            'x-rapidapi-host': Config.WEATHER_API_HOST,
+        }
 
-    return Response(status=404)
+        response = requests.request("GET", Config.WEATHER_API_URL, headers=headers, params=querystring)
+
+        if response.status_code == 200:
+            data = response.json()
+            weather = data['list'][0]
+            weather_data.append(weather)
+            print(weather_data)
+
+        else:
+            return Response(status=response.status_code)
+
+        if index == len(cities) - 1:
+            return render_template("weather.html", weather_data=weather_data)
+
+        continue
+
+
+@app.route('/search_location', methods=['POST'])
+def search_by_location():
+
+    pass
 
 
 if __name__ == '__main__':
